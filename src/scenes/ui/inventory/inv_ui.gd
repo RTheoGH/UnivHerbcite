44af80c54644917eb985cd4dc96571f12c46d6ae
TheoR <reynier.theo@gmail.com>
2025-10-25 15:08:59 +0200

    Infos items inventaire

--- src/scenes/ui/inventory/inv_ui.gd
+++ src/scenes/ui/inventory/inv_ui.gd
@@ -2,11 +2,18 @@ extends Control
 
 
 @onready var slots : Array = $PanelContainer/MarginContainer/HBoxContainer.get_children()
+@onready var info_panels = [$Infos/P1, $Infos/P2, $Infos/P3]
 
 var is_open = false
 
+var slot_textures = {
+	"base": preload("res://assets/graphical/ui/base_case.png"),
+	"active": preload("res://assets/graphical/ui/active_case.png")
+}
+
 func _ready():
 	close()
+	$Infos.hide()
 	
 func _process(_delta):
 	if Input.is_action_just_pressed("inventory") and !Global.isPaused:
@@ -16,15 +23,16 @@ func _process(_delta):
 			if !Global.is_craft_ui_open:
 				open()
 	
-	change_current_slot()
-
 	if Input.is_action_just_pressed("pause"):
 		close()
+	
+	change_current_slot()
 
 func update_slides():
 	#print(slots[0])
 	for i in range(min(Global.player_inventory.items.size(),slots.size())):
 		slots[i].update_inventory_visual(Global.player_inventory.items[i])
+
 
 func open():
 	update_slides()
@@ -39,29 +47,28 @@ func close():
 	
 func change_current_slot():
 	if Input.is_action_just_pressed("slot_left"):
-		if Global.inv_current_slot == 0:
-			Global.inv_current_slot = 2
-		else:
-			Global.inv_current_slot -= 1
+		Global.inv_current_slot = (Global.inv_current_slot - 1) % slots.size()
 	if Input.is_action_just_pressed("slot_right"):
-		if Global.inv_current_slot == 2:
-			Global.inv_current_slot = 0
-		else:
-			Global.inv_current_slot += 1
+		Global.inv_current_slot = (Global.inv_current_slot + 1) % slots.size()
 	
+	update_infos()
+	update_slot_textures()
+
+func _on_inventory_ui_slot_mouse_entered(slot_index: int) -> void:
+	Global.inv_current_slot = slot_index
+	update_infos()
+
+func update_slot_textures():
 	for i in range(slots.size()):
-		var texture_rect = slots[i].get_node("TextureRect")
+		slots[i].get_node("TextureRect").texture = slot_textures["active"] if i == Global.inv_current_slot else slot_textures["base"]
 
-		if i == Global.inv_current_slot:
-			texture_rect.texture = load("res://assets/graphical/ui/active_case.png")
-		else:
-			texture_rect.texture = load("res://assets/graphical/ui/base_case.png")
-
-func _on_inventory_ui_slot_mouse_entered() -> void:
-	Global.inv_current_slot = 0
-
-func _on_inventory_ui_slot_2_mouse_entered() -> void:
-	Global.inv_current_slot = 1
-
-func _on_inventory_ui_slot_3_mouse_entered() -> void:
-	Global.inv_current_slot = 2
+func update_infos():
+	if Global.inv_current_slot < Global.player_inventory.items.size() and Global.player_inventory.items[Global.inv_current_slot] != null:
+		var item = Global.player_inventory.items[Global.inv_current_slot]
+		$Infos/Texte.text = str(InventoryItem.InventoryItemType.find_key(item.type)) + "\n" + item.description
+		$Infos.show()
+		
+		for j in range(info_panels.size()):
+			info_panels[j].visible = (j == Global.inv_current_slot)
+	else:
+		$Infos.hide()
