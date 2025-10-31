1c594b12d9ca560006b4bd91d32fffe8765b5416
VigKillian <viguier.killian05@gmail.com>
2025-10-31 23:07:02 +0100

    placement romarin + fermer les ui + plus de pot bleue dans l'inv

--- src/scenes/tests/scene/iteractable_ui.gd
+++ src/scenes/tests/scene/iteractable_ui.gd
@@ -1,8 +1,11 @@
 extends Interactable
 
+func _ready() -> void:
+	if $"../Control/Button":
+		$"../Control/Button".button_up.connect(close_ui)
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed("pause") or Input.is_action_just_pressed("inventory"):
 		$"../Control".hide()
 		Global.is_interactable_ui_open = false
 
@@ -11,9 +14,10 @@ func on_interaction():
 	$"../Control".show()
 	Global.is_interactable_ui_open = true
 
-
-
 func _on_area_exited(area: Area3D) -> void:
 	if is_instance_of(area.get_parent(), RayCast3D):
-		$"../Control".hide()
-		Global.is_interactable_ui_open = false
+		close_ui()
+
+func close_ui():
+	$"../Control".hide()
+	Global.is_interactable_ui_open = false
