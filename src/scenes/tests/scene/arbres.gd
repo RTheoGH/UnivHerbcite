20daccb93ba41de92eed2bf7b38dcfeeb42d4547
VigKillian <viguier.killian05@gmail.com>
2025-11-01 09:46:07 +0100

    Tentative d'alpha sur les billboards d'arbres + narration recette zone 1

--- src/scenes/tests/scene/arbres.gd
+++ src/scenes/tests/scene/arbres.gd
@@ -1,22 +1,31 @@
 extends StaticBody3D
 
-@export var distance_haute_treshold: float = 100.0
-var distance_basse_treshold: float # hysteresis
+@export var distance_haute_threshold: float = 100.0
+var distance_basse_threshold: float # hysteresis
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	distance_basse_treshold = distance_haute_treshold - 5.0
+	distance_basse_threshold = distance_haute_threshold - 5.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var active_cam = get_viewport().get_camera_3d()
-	if $"MultiSprite3D" and active_cam:
-		if (active_cam.global_position - global_position).length_squared() < distance_basse_treshold**2:
+	if $"Sprite3D" and active_cam:
+		if (active_cam.global_position - global_position).length_squared() < distance_basse_threshold**2:
 			$TreeBas.show()
 			$TreeHaut.show()
-			$"MultiSprite3D".hide()
-		elif (active_cam.global_position - global_position).length_squared() > distance_haute_treshold**2:
+			$"Sprite3D".hide()
+		elif (active_cam.global_position - global_position).length_squared() > distance_haute_threshold**2:
 			$TreeBas.hide()
 			$TreeHaut.hide()
-			$"MultiSprite3D".show()
+			$"Sprite3D".show()
+		#else:
+			#var taux_alpha =  (distance_haute_threshold - (active_cam.global_position - global_position).length()) / distance_basse_threshold
+			#
+			#if distance_haute_threshold < 100.0:
+				#print(taux_alpha)
+				#print($TreeBas.material_override.get_shader_parameter("opacity"))
+			#$TreeBas.material_override.set_shader_parameter("opacity", taux_alpha)
+			##$TreeHaut.multimesh.mesh.material.set_shader_parameter("albedo:a", 1.0-taux_alpha)
+			#$"Sprite3D".modulate.a = 1.0-taux_alpha
--- src/scenes/tests/scene/iteractable_ui.gd
+++ src/scenes/tests/scene/iteractable_ui.gd
@@ -3,6 +3,8 @@ extends Interactable
 func _ready() -> void:
 	if $"../Control/Button":
 		$"../Control/Button".button_up.connect(close_ui)
+	if $"../Control".has_signal("ui_opened"):
+		$"../Control".ui_opened.connect(narrate_recipe_zone_1)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause") or Input.is_action_just_pressed("inventory"):
@@ -21,3 +23,7 @@ func _on_area_exited(area: Area3D) -> void:
 func close_ui():
 	$"../Control".hide()
 	Global.is_interactable_ui_open = false
+
+func narrate_recipe_zone_1():
+	await get_tree().create_timer(1.0).timeout
+	Global.narrate("La recette", "J'ai découvert une recette étrange. À quoi pourrait-elle servir ?")
