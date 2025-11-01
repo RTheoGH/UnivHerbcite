f3477ffa2ac44e967ce3e106d31504596eaa3cef
TheoR <reynier.theo@gmail.com>
2025-11-01 13:27:00 +0100

    tiens le k

--- src/classes/interactable.gd
+++ src/classes/interactable.gd
@@ -7,7 +7,7 @@ class_name Interactable
 var cooldown
 #@export var item : InventoryItem
 
-signal on_interaction_signal
+signal on_interaction_signal(area: Area3D)
 
 func _ready() -> void:
 	cooldown = get_tree().create_timer(0)
