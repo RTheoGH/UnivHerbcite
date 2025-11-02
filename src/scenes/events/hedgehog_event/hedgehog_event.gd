0932b1877b33fe951e2fa7e183d8ee5a6ff8c0b4
Sloth-King <andrew.mansour24@gmail.com>
2025-11-02 14:41:15 +0100

    Added second event trigger + fix collision spheres

--- src/scenes/events/hedgehog_event/hedgehog_event.gd
+++ src/scenes/events/hedgehog_event/hedgehog_event.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
-@export var speed = 10
+@export var speed = 0.5
 
 @onready var path : Path3D = $Path3D
 @onready var path_to_follow : PathFollow3D = $Path3D/PathFollow3D
 
 @onready var anim_player : AnimationPlayer = $Sketchfab_Scene/AnimationPlayer
+
+@onready var root_scene : Node3D = $Sketchfab_Scene
+
 
 var run_away = false
 var run_direction = Vector3(0,0,0)
@@ -19,11 +22,12 @@ func _process(delta: float) -> void:
 	if run_away:
 		# TODO Handle no path case
 		if path_to_follow.progress_ratio < 0.98 :
-			path_to_follow.progress += speed * delta
-			position = path_to_follow.position 
-			rotation.y = path_to_follow.rotation.y + deg_to_rad(180)
+			path_to_follow.progress_ratio += speed * delta
 			
-			anim_player.play("AnimalArmature|Gallop")
+			root_scene.position = path_to_follow.position
+			
+			root_scene.rotation.y = path_to_follow.rotation.y + deg_to_rad(180)
+			anim_player.play("Take 01")
 			
 		else:
 			run_away = false
